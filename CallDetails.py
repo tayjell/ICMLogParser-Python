5933b165cb2fb3c8ca933e755c19fdc113e9cf8b
tayjell <tayjell@gmail.com>
2018-09-12 20:40:15 -0400

    Add files via upload

---
+++ CallDetails.py
@@ -0,0 +1,30 @@
+class callDetails:
+
+   def __init__(self):
+      self.callVar4 = ""
+      self.callVar6 = ""
+      self.callVarEcc29 = ""
+      self.callVarEcc30 = ""
+      self.callVarEcc31 = ""
+      self.callTime = ""
+      self.sessionID = ""
+	
+      def setVar4(self, var4):
+         self.callVar4 = var4
+
+	
+      def setVar6(self, var6):
+         self.callVar6 = var6
+
+	
+      def setEcc29(self, ecc29):
+         self.callVarEcc29 = ecc29
+	
+	
+      def setEcc30(self, ecc30):
+         self.callVarEcc30 = ecc30;	
+	
+	
+      def setEcc31(self, ecc31):
+         self.callVarEcc31 = ecc31;	
+	
